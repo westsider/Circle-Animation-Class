d43342adaf5d3a3adbe829911ed2d31221b3ff55
Warren Hansen <whansen1@me.com>
2018-08-22 09:23:15 -0500

    Initial Commit

---
+++ Circle Animation Class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Circle Animation Class
+//
+//  Created by Warren Hansen on 8/22/18.
+//  Copyright © 2018 Warren Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
